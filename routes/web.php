f6fcfd13e476ec16310603b77263ad71bebf5088
sajaeli <sajaelian293@gmail.com>
2021-04-19 21:01:44 +0100

    third

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Rout::get('tasks',function(){
-    $tasks= [
-    'Task 1',
-    'Task 2',
-    'Task 3',
-    ];
-    return view('tasks',compact('task'));
+Route::get('tasks',function(){
+    $tasks = DB ::table ('tasks')->get();
+    return view('tasks',compact('tasks'));
 });
-Rout::get('show/{id}',function($id){
-    $tasks = [
-        'first-task' => 'Task 1',
-        'second-task' => 'Task 2',
-        'Third-task' => 'Task 3',
-    ];
-    $task = $task[$id] ;
-    return view('show',compact('task'));
+Rout::get('/show/{id}', function ($id) {
+    $task = DB::table('std')->find($id);
+    return view('show',compact(('task')));
 });
